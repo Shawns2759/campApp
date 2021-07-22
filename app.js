2458dacab7d1b877b0d4ce4b4351a2950a84f3ed
Shawn Schneider <shawns2759@gmail.com>
2021-07-22 16:12:35 -0700

    added flash/sessions and refactored for express router and added static folders/assets

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const Campground = require('./models/campground')
+// const Campground = require('./models/campground')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError.js')
 const { campgroundSchema, reviewSchema } = require('./schemas')
-const Review = require('./models/review')
+const session = require('express-session')
+const flash = require('connect-flash')
+// const Review = require('./models/review')
+const campgrounds = require('./routes/campgrounds')
+const reviews = require('./routes/reviews')
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
+
 });
 
 const db = mongoose.connection;
@@ -30,94 +37,34 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 //creates alias for query string to make a put request
 app.use(methodOverride('_method'))
-
-//mongoose validation middlewear
-const validateCampground = (req, res, next) => {
-    //gets error form campgroundSchema aka validation schema with joi
-    const { error } = campgroundSchema.validate(req.body)
-    if (error) {
-        console.log(error)
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(error.message, 400)
-    } else {
-        next()
+//serves static files from public
+app.use(express.static(path.join(__dirname, 'public')) )
+const sessionConfig = {
+    secret: 'secret',
+    resave: false, 
+    saveUninitialized: true,
+    cookie: {
+        //cookie settings expires in a week max age: week http only dosent allow css
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body)
-    if (error) {
-        console.log(error)
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(error.message, 400)
-    } else {
-        next()
-    }
-}
+//makes session and configs it to sessionConfig
+app.use(session(sessionConfig))
 
-//routes
+app.use(flash())
 
-app.get('/', (req, res) => {
-    res.render('/home')
+app.use((req, res, next) => {
+    //sets a local var in views sucess is === success thrown after new form completion check campground routes new 
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
 })
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({})
-    res.render('campgrounds/index.ejs', {campgrounds})
-})
+//imports campgrounds router form campgrounds and prefixes it with '/campgrounds'
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews',reviews)
 
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new.ejs')
-})
-app.post('/campgrounds', validateCampground,catchAsync(async (req, res, next) => {
-        const newCamp = new Campground(req.body.campground);
-        console.log(req.body.campground)
-        await newCamp.save()
-        res.redirect(`campgrounds/${newCamp.id}`)
-}))
-app.get('/campgrounds/:id',  catchAsync(async (req, res) => {
-    const {id} = req.params
-    const campground = await Campground.findById(id).populate('reviews')
-    res.render('campgrounds/show.ejs', {campground})
-}))
-app.get('/campgrounds/:id/edit',  catchAsync(async (req, res) => {
-    const {id} = req.params
-    const campground = await Campground.findById(id)
-    res.render('campgrounds/edit', {campground})
-}))
-app.put('/campgrounds/:id',  validateCampground,catchAsync(async (req, res) => {
-    const { id } = req.params
-    const {location, title, description, image, price} = req.body.campground
-    const updatedCamp = await Campground.findByIdAndUpdate(id, {
-        location: location,
-        title: title,
-        descriiption: description,
-        image: image, 
-        price: price
-    },{new:true})
-    res.redirect(`/campgrounds/${updatedCamp.id}`)
-}))
-app.delete('/campgrounds/:id',  catchAsync(async (req, res) => {
-    const {id} = req.params
-    await Campground.findByIdAndDelete(id)
-    res.redirect(`/campgrounds`)
-}))
-app.post('/campgrounds/:id/reviews', validateReview ,catchAsync(async (req, res) => {
-    const { id } = req.params
-    const campground = await Campground.findById(id).populate('reviews')
-    console.log(req.body.review)
-    const review = new Review(req.body.review)
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    res.redirect(`/campgrounds/${campground.id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => { 
-    const { id, reviewId } = req.params
-    //finds camp by id and pulls every instance of reviewID out of reviews arr
-    const camp = await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
-    const review = await Review.findByIdAndDelete(reviewId)
-    res.redirect(`/campgrounds/${id}`)
-}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found 404', 404))
--- schemas.js
+++ schemas.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required().min(1).max(150),
-        price: Joi.number().required().min(1).max(50),
+        price: Joi.number().required().min(1),
         description: Joi.string().required().min(1).max(500),
         image: Joi.string().required().min(1).max(150),
         location: Joi.string().required().min(1).max(50),
